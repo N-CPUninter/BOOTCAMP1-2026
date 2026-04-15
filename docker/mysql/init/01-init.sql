b974fe8c9ec3f5d4a9afcff0ef2a83a9d7b14e50
felixnogueira <felixnogueiradev@gmail.com>
2026-04-15 19:44:12 -0300

    Commit da criação da tabela Alerta

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -3,17 +3,33 @@
 -- Exemplo simples de inserção de dados.
 -- Mostraremos 2 tabelas com 3 colunas cada e relacionadas entre si.
 USE medalerta;
-CREATE TABLE IF NOT EXISTS users (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-		name VARCHAR(255),
-		email VARCHAR(255)
+-- depois de fazer o clone do projeto que estamos usando (atenção, não é o da faculdade!) voce pode abrir no vs code o projeto, como ta aberto
+-- aqui. no futuro, quando vc for iniciar o desenvolvimento, depois de fazer o clone e se tem outras pessoas ja trabalhando no projeto,
+-- o ideal é sempre dar git pull antes. pra isso, abra um novo terminal ^
+
+-- o meu n fez nada pq nao tem nada pra puxar, n tem nada novo. seguimos. agora vc pode começar o desenvolvimento:
+CREATE TABLE Alerta(
+	idAlerta INT auto_increment NOT NULL,
+    idHorarioMedicamento INT NOT NULL,
+    tempoMinutos INT NOT NULL,
+    statusAlerta ENUM('emitido', 'pendente', 'confirmado'),
+    ativo BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (idAlerta),
+    FOREIGN KEY (idHorarioMedicamento) REFERENCES HorarioMedicamento(idHorarioMedicamento)
 );
-CREATE TABLE IF NOT EXISTS posts (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-		title VARCHAR(255),
-		content TEXT,
-		user_id INT,
-		FOREIGN KEY (user_id) REFERENCES users(id)
-);
-INSERT IGNORE INTO users (name, email) VALUES ('Alice', 'alice@example.com');
-INSERT IGNORE INTO posts (title, content, user_id) VALUES ('Primeira Postagem', 'Conteúdo da primeira postagem.', 1);
+
+--adicionadas as modificacoes, o ideal seria testar o codigo. caso nao tenha feito ainda, segue o que deve ser feito:
+-- copia do .env: cp .env.exemplo .env
+-- rodar o container (o prof explicou pelo devcontainers la mas so sei fazer na mao)
+-- docker compose up -d
+
+-- pode demorar mais se for a primeira vez.
+-- alias, deveria ser --build
+-- docker compose up --build -d (o -d é pra liberar o terminal p vc usar, pode fazer sem e criar um novo terminal, assim vc consegue ver os 
+-- logs em tempo real tb)
+
+-- com o docker criado, vamos testar o comando acima no mysql workbench
+
+--nao deu certo :D acho que pq nao salvei o arquivo. tentamos de novo. pra parar o docker, docker compose down
+
+--seta pra cima no terminal mostra os ultimos comandos.
